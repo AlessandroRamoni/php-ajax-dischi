233bf003510780a163406757edeb5c08936f8c3d
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-17 17:29:55 +0200

    create folder

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-ajax-dischi</title>
+</head>
+<body>
+    
+<?php
+
+$dischi = [
+    [  
+    "immagine" => "...",
+    "titolo" => "A caso",
+    "genere" => "Rock",
+    "autore" => "Bon jovi",
+    ],
+    [  
+    "immagine" => "...",
+    "titolo" => "Ciao",
+    "genere" => "Rock",
+    "autore" => "Sting",
+    ],
+    [  
+    "immagine" => "...",
+    "titolo" => "Arrivederci",
+    "genere" => "Rock",
+    "autore" => "Queen",
+    ],
+    [  
+    "immagine" => "...",
+    "titolo" => "Speranza",
+    "genere" => "Pop",
+    "autore" => "883",
+    ],
+];
+
+?>
+
+
+</body>
+</html>
